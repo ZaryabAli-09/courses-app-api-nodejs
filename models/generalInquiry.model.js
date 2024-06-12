da2352cb2a842abedb676ecfcc632e012c249896
Zaryab <zaryabkhan248@gmail.com>
2024-06-13 03:04:38 +0500

    adding timestamps in general inquiry model

--- models/generalInquiry.model.js
+++ models/generalInquiry.model.js
@@ -1,28 +1,31 @@
 import mongoose from "mongoose";
 // id:ObjectId, name:string, email:string, inquiry:string, new:boolean:bydefault=true, reply:boolean:bydefault=false, date
 
-const generalInquirySchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const generalInquirySchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    isnew: {
+      type: Boolean,
+      default: true,
+    },
+    reply: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  isnew: {
-    type: Boolean,
-    default: true,
-  },
-  reply: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 const GeneralInquiries = mongoose.model(
   "GeneralInquiries",
